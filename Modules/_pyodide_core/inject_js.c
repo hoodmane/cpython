15b7100c5e22d11c02847608039579f82ba70373
Hood Chatham <roberthoodchatham@gmail.com>
2025-05-06 18:22:21 -0400

    Use #embed magic to inject jslib.js file

---
+++ Modules/_pyodide_core/inject_js.c
@@ -0,0 +1,20 @@
+#include <emscripten.h>
+
+__attribute__((used))
+__attribute__((section("em_js"),
+               aligned(1))) char __em_js__pyodide_js_init[] = {
+        // "()=>{}" in ascii.
+    40, 41, 60, 58, 58, 62, 123, 125,
+    #embed "jslib.js"
+    // Null byte to end string
+    , 0
+};
+
+void
+pyodide_js_init(void) EM_IMPORT(pyodide_js_init);
+
+EMSCRIPTEN_KEEPALIVE void
+pyodide_export(void)
+{
+  pyodide_js_init();
+}
